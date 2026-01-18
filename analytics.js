daaad26192c08a8be250b56f5efcffbba12a4b9b
BJ Beier <bjbeier@gmail.com>
2026-01-18 18:05:27 -0500

    correct GA code set

--- analytics.js
+++ analytics.js
@@ -1,4 +1,4 @@
 window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
+function gtag() { dataLayer.push(arguments); }
 gtag('js', new Date());
-gtag('config', 'G-353553686');
+gtag('config', 'G-2RDT4CRT92');
