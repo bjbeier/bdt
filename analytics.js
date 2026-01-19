d45ee257f06b00eb5f3db6ab8de998ce84d8665a
BJ Beier <bjbeier@gmail.com>
2026-01-18 20:08:30 -0500

    feat: Implement a cookie consent banner and conditionally load Google Analytics based on user consent.

--- analytics.js
+++ analytics.js
@@ -1,4 +1,11 @@
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-gtag('config', 'G-2RDT4CRT92');
+window.loadTracking = function () {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-2RDT4CRT92";
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', 'G-2RDT4CRT92');
+};
