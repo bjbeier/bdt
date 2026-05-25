cf6cc195679f9b90a26540f770c21e434cd30a6a
BJ Beier <bjbeier@gmail.com>
2026-05-25 18:10:27 -0400

    Update production Google Analytics ID to G-2RDT4CRT92

--- analytics.js
+++ analytics.js
@@ -1,11 +1,11 @@
 window.loadTracking = function () {
     const script = document.createElement('script');
     script.async = true;
-    script.src = "https://www.googletagmanager.com/gtag/js?id=G-70WBG7VJPR";
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-2RDT4CRT92";
     document.head.appendChild(script);
 
     window.dataLayer = window.dataLayer || [];
     function gtag() { dataLayer.push(arguments); }
     gtag('js', new Date());
-    gtag('config', 'G-70WBG7VJPR');
+    gtag('config', 'G-2RDT4CRT92');
 };
